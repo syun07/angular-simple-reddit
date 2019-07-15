fba589c68b27724e1cc880e6b42352bf97e05b50
Sarah Yun <sarahyun7@gmail.com>
2019-07-15 18:17:11 -0400

    refactor to create components with passed in data

--- src/app/user-item/user-item.component.ts
+++ src/app/user-item/user-item.component.ts
@@ -1,16 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-user-item',
   templateUrl: './user-item.component.html',
   styleUrls: ['./user-item.component.css']
 })
-export class UserItemComponent implements OnInit {
-  name: string;
 
-  constructor() { 
-    this.name = 'Sarah';
-  }
+export class UserItemComponent implements OnInit {
+  @Input() name: string;
+
+  constructor() { }
 
   ngOnInit() {
   }
