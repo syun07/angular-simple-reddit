5fe045b98ff276fd66d3ed4ec44942245dcfdaa2
Sarah Yun <sarahyun7@gmail.com>
2019-07-14 20:54:11 -0400

    rendered template with set property

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,13 @@ import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
 import { HelloWorldComponent } from './hello-world/hello-world.component';
+import { UserItemComponent } from './user-item/user-item.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HelloWorldComponent
+    HelloWorldComponent,
+    UserItemComponent
   ],
   imports: [
     BrowserModule,
