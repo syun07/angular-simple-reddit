93fdf344351b217f31f19ba6dae42e2c86e05322
Sarah Yun <sarahyun7@gmail.com>
2019-07-21 18:23:09 -0400

    add notes

--- src/app/article/article.component.ts
+++ src/app/article/article.component.ts
@@ -7,6 +7,10 @@ import { Component, OnInit, HostBinding } from '@angular/core';
 })
 export class ArticleComponent implements OnInit {
   @HostBinding('attr.class') cssClass='row';
+  // we're using Semantic UI, which provides a CSS class for rows called 'row'
+  // @HostBinding is imported from the package @angular/core, encapsulates the app-article markup within our component
+  // allows us to configure our host element from within the component
+
   votes: number;
   title: string;
   link: string;
@@ -15,6 +19,7 @@ export class ArticleComponent implements OnInit {
     this.title = "Angular 2";
     this.link = "http://angular.io";
     this.votes = 10;
+    // set default attributes
   }
 
   voteUp() {
